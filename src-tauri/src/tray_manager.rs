4e5b59de52b4f881570972b85ea2df8ca23e37ee
zwh <2928776248@qq.com>
2026-05-02 15:17:07 +0800

    feat: implement system tray manager
    - Add tray icon with show/quit menu
    - Left-click shows/focuses main window
    - Right-click shows context menu
    - Tooltip displays current game status

--- src-tauri/src/tray_manager.rs
+++ src-tauri/src/tray_manager.rs
@@ -1,8 +1,44 @@
-// TODO: implement tray manager
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+    AppHandle, Manager,
+};
 
-use tauri::AppHandle;
+pub fn setup_tray(app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
+    let show_i = MenuItem::with_id(app, "show", "显示窗口", true, None::<&str>)?;
+    let quit_i = MenuItem::with_id(app, "quit", "退出", true, None::<&str>)?;
 
-pub fn setup_tray(_handle: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: implement tray manager
+    let menu = Menu::with_items(app, &[&show_i, &quit_i])?;
+
+    let _tray = TrayIconBuilder::new()
+        .icon(app.default_window_icon().unwrap().clone())
+        .menu(&menu)
+        .tooltip("GAL Tracker")
+        .on_menu_event(|app, event| {
+            match event.id.as_ref() {
+                "show" => {
+                    if let Some(window) = app.get_webview_window("main") {
+                        let _ = window.show();
+                        let _ = window.set_focus();
+                    }
+                }
+                "quit" => {
+                    app.exit(0);
+                }
+                _ => {}
+            }
+        })
+        .on_tray_icon_event(|tray, event| {
+            if let tauri::tray::TrayIconEvent::Click { button, .. } = event {
+                if button == tauri::tray::MouseButton::Left {
+                    if let Some(window) = tray.app_handle().get_webview_window("main") {
+                        let _ = window.show();
+                        let _ = window.set_focus();
+                    }
+                }
+            }
+        })
+        .build(app)?;
+
     Ok(())
 }
